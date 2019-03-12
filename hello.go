a7660a7bda010ac005a983f1f1fa07a4a3b067ea
Your Name <you@example.com>
2019-03-12 10:33:05 +0200

    snigel

---
+++ hello.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+        "fmt"
+)
+var namn string
+func main() {
+	namn = "Vini"
+
+	switch namn {
+		case "Anna":
+			fmt.Println("Ha det bra!-Anton")
+		case "Vini":
+			fmt.Println("Kasta en denkku please")
+		case "Anton":
+			fmt.Println("Ayy Belmen I no join freda hope u have good time tho")
+		case "Axel":
+			fmt.Println("Ur father big cheat no get lost")
+		case "Levi":
+			fmt.Println("Elämä hymyilee ku on big krunk ys")}
+}
